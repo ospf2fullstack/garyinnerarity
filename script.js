1dc4ec1b120cbf2680fec73808aa6ff46b4abcd3
ospf2fullstack <garyinnerarity@gmail.com>
2021-08-24 21:03:44 -0500

    Cool

--- script.js
+++ script.js
@@ -121,3 +121,23 @@ particlesJS("particles-js", {
 const hours = document.querySelector('.hours');
 const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds');
+
+$(function () {
+  $("#file-manager").dxFileManager({
+      name: "fileManager",
+      fileProvider: new DevExpress.fileProviders.Remote({
+          endpointUrl: "https://js.devexpress.com/Demos/Mvc/api/file-manager-file-system-scripts"
+      }),
+      permissions: {
+          download: true
+          // uncomment the code below to enable file/directory management
+          /* create: true,
+          copy: true,
+          move: true,
+          remove: true,
+          rename: true,
+          upload: true */
+      },
+      allowedFileExtensions: [".html", ".pdf", ".css", ".txt"]
+  });
+});
