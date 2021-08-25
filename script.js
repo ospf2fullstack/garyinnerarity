9d9ee965c128bafd8c5f9021d7510c2165086b5e
ospf2fullstack <garyinnerarity@gmail.com>
2021-08-24 21:15:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -117,23 +117,3 @@ particlesJS("particles-js", {
   "retina_detect": true
 });
 
-/* File System */
-$(function () {
-  $("#file-manager").dxFileManager({
-      name: "fileManager",
-      fileProvider: new DevExpress.fileProviders.Remote({
-          endpointUrl: "https://js.devexpress.com/Demos/Mvc/api/file-manager-file-system-scripts"
-      }),
-      permissions: {
-          download: true
-          // uncomment the code below to enable file/directory management
-          /* create: true,
-          copy: true,
-          move: true,
-          remove: true,
-          rename: true,
-          upload: true */
-      },
-      allowedFileExtensions: [".html", ".pdf", ".css", ".txt"]
-  });
-});
