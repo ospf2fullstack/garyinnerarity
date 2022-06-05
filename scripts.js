ea76582be5706e965250a39609fe702e23f89359
Gary Innerarity <garyinnerarity@gmail.com>
2022-06-05 16:47:41 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,26 +13,6 @@ async () => {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
-
-      const h1 = document.createElement('h1')
-      h1.textContent = movie.title
-
-      const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
-
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
-  }
 }
 
 request.send()
