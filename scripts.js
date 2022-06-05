a7f2229892f7ef6504dd293bda8e9043b3879c6e
Gary Innerarity <garyinnerarity@gmail.com>
2022-06-05 16:51:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY});
 async () => {
     const response = await notion.databases.query({
         const pageId = 'ba02ced6db5c496989c61072ae0adabd';
-        const response = await notion.pages.retrieve({page_id: pageId});
+        const response = await notion.pages.retrieve({page_id: 'ba02ced6db5c496989c61072ae0adabd'});
         console.log(response);
 })();
 
